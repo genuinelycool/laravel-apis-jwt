50b378bce4366be104bc102c3010e5e5b51e9c61
genuinelycool <genuinelycool@gmail.com>
2025-10-20 21:38:53 +0545

    skeleton for ApiController methods and define api routes

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -4,8 +4,32 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class ApiController extends Controller
 {
-    //
+    // Register API [name, email, password, password_confirmation]
+    public function register() {
+
+    }
+
+    // Login API [email, password]
+    public function login() {
+
+    }
+
+    // Profile API
+    public function profile() {
+
+    }
+
+    // Refresh API
+    public function refresh() {
+
+    }
+
+    // Logout API
+    public function logout() {
+
+    }
 }
